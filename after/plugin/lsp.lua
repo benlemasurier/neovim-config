0141c096438d6ca3e94276f113f58646f755ea02
Ben LeMasurier <ben.lemasurier@gmail.com>
2023-02-06 10:57:29 -0700

    lsp: set completeopt

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,12 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+lsp.setup_nvim_cmp({
+    completion = {
+	completeopt = 'menu,menuone,noselect'
+    },
+})
+
 lsp.ensure_installed({
     'clangd',
     'gopls',
