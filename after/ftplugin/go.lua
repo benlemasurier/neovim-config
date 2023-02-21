a81ff15fc1f4c60a151541d960791f07ad360366
Ben LeMasurier <ben.lemasurier@gmail.com>
2023-02-21 14:46:15 -0700

    golint, comment

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -6,3 +6,9 @@ vim.keymap.set('n', '<leader>b', ':GoBuild ./...<CR>')
 
 -- test
 vim.keymap.set('n', '<leader>t', ':GoTest ./...<CR>')
+
+-- lint
+vim.keymap.set('n', '<leader>l', ':GoLint<CR>')
+
+-- auto-gen comment
+vim.keymap.set('n', '<leader>gc', ":lua require('go.comment').gen()<CR>")
